cafae0403757fa48903e43d5beea9bb38979bef8
SOFT01 <60617938+edragalzeran@users.noreply.github.com>
2020-07-10 12:10:51 +0200

    Scripts

---
+++ scripts/PreloadImage.js
@@ -0,0 +1,11 @@
+﻿var funcions = funcions || {}
+
+funcions.preloadImages = function (imageSources) {
+    imageSources.forEach(i => {
+    const linkEl = document.createElement('link');
+linkEl.setAttribute('rel', 'preload');
+linkEl.setAttribute('href', i);
+linkEl.setAttribute('as', 'image');
+document.head.appendChild(linkEl);
+});
+}
